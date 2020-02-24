e422f265052831722bde71178848b355b91f60ef
Artem_Zabirov <qwertysairex@gmail.com>
2020-02-24 13:35:47 +0300

    Updates

---
+++ precache-manifest.322615751bded8e626d2866f46e8f92d.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7656e4f5cdb1328151c79e2e7ba965af",
+    "url": "/testtask/index.html"
+  },
+  {
+    "revision": "165edf8f1016dcecd6c8",
+    "url": "/testtask/static/css/main.57565879.chunk.css"
+  },
+  {
+    "revision": "3e636f53ffacc201dcaf",
+    "url": "/testtask/static/js/2.7651ba47.chunk.js"
+  },
+  {
+    "revision": "928d7b5eb39d16fe9a880722c974b51e",
+    "url": "/testtask/static/js/2.7651ba47.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "165edf8f1016dcecd6c8",
+    "url": "/testtask/static/js/main.125d589f.chunk.js"
+  },
+  {
+    "revision": "634863a150b8d0595d6a",
+    "url": "/testtask/static/js/runtime-main.3d78e089.js"
+  },
+  {
+    "revision": "f5443d74025f2a6069e0ca0142c08a05",
+    "url": "/testtask/static/media/banner-photo.f5443d74.jpg"
+  },
+  {
+    "revision": "146e29265bcc680129e263a9478dca00",
+    "url": "/testtask/static/media/logo.146e2926.svg"
+  },
+  {
+    "revision": "3cdf78971c869b16b95152be7b6922e5",
+    "url": "/testtask/static/media/man-laptop-v1.3cdf7897.svg"
+  }
+]);
